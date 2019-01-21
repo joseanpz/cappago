9a70d8f04487f8e2e8fa05164adc26a187ce43ed
Unknown <jacob.mendozaha@gmail.com>
2019-01-21 13:32:56 -0600

    datatable

--- js/main.js
+++ js/main.js
@@ -17,6 +17,11 @@ const EvalList = { template: '#list-template' };
 const EvalForm = { template: '#form-template' };
 const EvalUpdateForm = { template: '#updateform-template' };
 const EvalDetail = { template: '#detail-template' };
+ 
+Vue.component('DataTable', {
+  props:  ['data', 'columns'],
+  template: '#table-template'
+})
 
 
 const User = { template: '<div>User {{ $route.params.uname }}</div>' };
@@ -47,6 +52,48 @@ const router = new VueRouter({
 // Make sure to inject the router with the router option to make the
 // whole app router-aware.
 const app = new Vue({
+  
+  data() {
+            return {
+              data_table() {  
+                return {
+                  data: [
+                    { 'id': 1, 'first_name': 'Jesse', 'last_name': 'Simmons', 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
+                    { 'id': 2, 'first_name': 'John', 'last_name': 'Jacobs', 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
+                    { 'id': 3, 'first_name': 'Tina', 'last_name': 'Gilbert', 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
+                    { 'id': 4, 'first_name': 'Clarence', 'last_name': 'Flores', 'date': '2016-04-10 10:28:46', 'gender': 'Male' },
+                    { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016-12-06 14:38:38', 'gender': 'Female' }
+                  ],
+                  columns: [
+                    {
+                        field: 'id',
+                        label: 'ID',
+                        width: '40',
+                        numeric: true
+                    },
+                    {
+                        field: 'first_name',
+                        label: 'First Name',
+                    },
+                    {
+                        field: 'last_name',
+                        label: 'Last Name',
+                    },
+                    {
+                        field: 'date',
+                        label: 'Date',
+                        centered: true
+                    },
+                    {
+                        field: 'gender',
+                        label: 'Gender',
+                    }
+                  ]
+                }            
+                
+              }
+            }
+  },
   router,
   /*computed: {
     username () {
