fd7ee6d085c2804284f5e77ee436eabc52d1e9bd
Jose Arce <joseangel.epzarce@gmail.com>
2019-01-21 16:28:30 -0600

    paginatied table

--- js/components/data-table.js
+++ js/components/data-table.js
@@ -1,6 +1,6 @@
 const DataTable = Vue.component('data-table', {
-  props:  ['data', 'columns', 'isPaginated', 'isPaginationSimple', 
-  'defaultSortDirection', 'currentPage', 'perPage'],
+  // props:  ['data', 'columns', 'isPaginated', 'isPaginationSimple', 
+  // 'defaultSortDirection', 'currentPage', 'perPage'],
   //template: '<span>Test</span>'
   template: `    
     <section>
@@ -62,5 +62,62 @@ const DataTable = Vue.component('data-table', {
                 </b-table-column>
             </template>
         </b-table>
-    </section>`
+    </section>`,
+  data() {
+		return {
+		  data: [
+                { 'id': 1, 'user': { 'first_name': 'Jesse', 'last_name': 'Simmons' } , 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
+                { 'id': 2, 'user': { 'first_name': 'John', 'last_name': 'Jacobs' }, 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
+                { 'id': 3, 'user': { 'first_name': 'Tina', 'last_name': 'Gilbert' }, 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
+                { 'id': 4, 'user': { 'first_name': 'Clarence', 'last_name': 'Flores' }, 'date': '2016-04-10 10:28:46', 'gender': 'Male' },
+                { 'id': 5, 'user': { 'first_name': 'Anne', 'last_name': 'Lee' }, 'date': '2016-12-06 14:38:38', 'gender': 'Female' },
+                { 'id': 1, 'user': { 'first_name': 'Jesse', 'last_name': 'Simmons' } , 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
+                { 'id': 2, 'user': { 'first_name': 'John', 'last_name': 'Jacobs' }, 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
+                { 'id': 3, 'user': { 'first_name': 'Tina', 'last_name': 'Gilbert' }, 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
+                { 'id': 1, 'user': { 'first_name': 'Jesse', 'last_name': 'Simmons' } , 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
+                { 'id': 2, 'user': { 'first_name': 'John', 'last_name': 'Jacobs' }, 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
+                { 'id': 3, 'user': { 'first_name': 'Tina', 'last_name': 'Gilbert' }, 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
+                { 'id': 1, 'user': { 'first_name': 'Jesse', 'last_name': 'Simmons' } , 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
+                { 'id': 2, 'user': { 'first_name': 'John', 'last_name': 'Jacobs' }, 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
+                { 'id': 3, 'user': { 'first_name': 'Tina', 'last_name': 'Gilbert' }, 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
+                { 'id': 1, 'user': { 'first_name': 'Jesse', 'last_name': 'Simmons' } , 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
+                { 'id': 2, 'user': { 'first_name': 'John', 'last_name': 'Jacobs' }, 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
+                { 'id': 3, 'user': { 'first_name': 'Tina', 'last_name': 'Gilbert' }, 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
+                { 'id': 1, 'user': { 'first_name': 'Jesse', 'last_name': 'Simmons' } , 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
+                { 'id': 2, 'user': { 'first_name': 'John', 'last_name': 'Jacobs' }, 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
+                { 'id': 3, 'user': { 'first_name': 'Tina', 'last_name': 'Gilbert' }, 'date': '2016-04-26 06:26:28', 'gender': 'Female' }
+                
+              ],
+          columns: [
+                {
+                field: 'id',
+                label: 'ID',
+                width: '40',
+                numeric: true
+                },
+                {
+                field: 'first_name',
+                label: 'First Name',
+                },
+                {
+                field: 'last_name',
+                label: 'Last Name',
+                },
+                {
+                field: 'date',
+                label: 'Date',
+                centered: true
+                },
+                {
+                field: 'gender',
+                label: 'Gender',
+                }
+            ],
+          isPaginated: true,
+          isPaginationSimple: false,
+          defaultSortDirection: 'asc',
+          currentPage: 1,
+          perPage: 5  
+		}
+	}   
 })
