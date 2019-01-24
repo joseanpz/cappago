94631e9d363bef825e26f9fff95b898ec9afb14f
Jose Arce <joseangel.epzarce@gmail.com>
2019-01-24 12:59:34 -0600

    read init

--- gs/api.js
+++ gs/api.js
@@ -1,14 +1,24 @@
 var user = new User(Session.getActiveUser());
   
-  //TODO: Validations 
-  if(user.is_authorized()) {
-    var template = HtmlService.createTemplateFromFile('index');
-    template.user_email = user.getEmail(); 
-    return template.evaluate();
-  }
-  else {
-    return HtmlService.createHtmlOutputFromFile('noacceso');
-  }
+//TODO: Validations 
+if(user.is_authorized()) {
+var template = HtmlService.createTemplateFromFile('index');
+template.user_email = user.getEmail(); 
+return template.evaluate();
+}
+else {
+return HtmlService.createHtmlOutputFromFile('noacceso');
+}
+
+
+
+function _read(sheetName, filters) {
+	var data = {};
+  	var sheet = db.getSheetByName(sheetName);
+  	  
+  	data.records = _readData(sheet);  
+  	return data;
+}
 
 
   
