89b8ed0748e5b226d6ecae772fa74b498ce0fb60
Unknown <jacob.mendozaha@gmail.com>
2019-02-08 15:09:38 -0600

    Formato resultado perfilador

--- js/components/resultado-perfilador-step.js
+++ js/components/resultado-perfilador-step.js
@@ -50,43 +50,43 @@ var ResultadoPerfiladorStep = Vue.component('resultado-perfilador-step',{
 					</header>
 					<div class="card-content">	
 						<div class="content columns">
-							<div class="column title-color">Simple:</div>
-							<div class="column title-color">Revolvente</div>
-						</div>
+							<div class="column title-color" style="border:1px solid #3a5fab;">Simple:</div>
+							<div class="column title-color" style="border:1px solid #3a5fab;">Revolvente</div>
+						</div> 
 						<div class="content columns">
-							<div class="column">Monto solicitado:</div>
+							<div class="column alinea_text">Monto solicitado:</div>
 							<div class="column">{{monto_solicitado.simple}}</div>
-							<div class="column">Monto solicitado:</div>
+							<div class="column alinea_text">Monto solicitado:</div>
 							<div class="column">{{monto_solicitado.revolvente}}</div>					
 						</div>
 						<div class="content columns">
-							<div class="column">Capacidad pago:</div>
+							<div class="column alinea_text">Capacidad pago:</div>
 							<div class="column">{{capacidad_pago.simple}}</div>
-							<div class="column">Capacidad pago:</div>
+							<div class="column alinea_text">Capacidad pago:</div>
 							<div class="column">{{capacidad_pago.revolvente}}</div>					
 						</div>
 						<div class="content columns">
-							<div class="column">Diferencia entre ingreso anual y deuda total:</div>
+							<div class="column alinea_text">Diferencia entre ingreso anual y deuda total:</div>
 							<div class="column">{{ingreso_vs_deuda.simple}}</div>
-							<div class="column">Diferencia entre ingreso anual y deuda total:</div>
+							<div class="column alinea_text">Diferencia entre ingreso anual y deuda total:</div>
 							<div class="column">{{ingreso_vs_deuda.revolvente}}</div>					
 						</div>
 						<div class="content columns">
-							<div class="column">Razon entre flujo y factor recuperacion de capital:</div>
+							<div class="column alinea_text">Razon entre flujo y factor recuperacion de capital:</div>
 							<div class="column">{{razon_flujo_rec_capital}}</div>
-							<div class="column">Razon entre flujo y tasa anual:</div>
+							<div class="column alinea_text">Razon entre flujo y tasa anual:</div>
 							<div class="column">{{razon_flujo_tasa}}</div>					
 						</div>
 						<div class="content columns">
-							<div class="column">Monto maximo:</div>
+							<div class="column alinea_text">Monto maximo:</div>
 							<div class="column">{{monto_maximo.simple}}</div>
-							<div class="column">Monto maximo:</div>
+							<div class="column alinea_text">Monto maximo:</div>
 							<div class="column">{{monto_maximo.revolvente}}</div>					
 						</div>
 						<div class="content columns">
-							<div class="column">Linea:</div>
+							<div class="column alinea_text">Linea:</div>
 							<div class="column">{{linea.simple}}</div>
-							<div class="column">Linea:</div>
+							<div class="column alinea_text">Linea:</div>
 							<div class="column">{{linea.revolvente}}</div>					
 						</div>
 					</div>
