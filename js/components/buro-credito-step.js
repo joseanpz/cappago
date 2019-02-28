06489bb0c283ef705ad29995e4cea4d4c5d59b4e
Unknown <jacob.mendozaha@gmail.com>
2019-02-28 11:56:36 -0600

    acomodo en buro, detalles

--- js/components/buro-credito-step.js
+++ js/components/buro-credito-step.js
@@ -10,139 +10,141 @@ var BuroCreditoStep = Vue.component('buro-credito-step', {
 					<div class="content">
 						<div class="columns">
 							<div class="column">
-								<div class="card">
-									<header class="header-sec-card">
-										<p class="card-header-title title-color">Segmento califica</p>
-									</header>
-									<div class="card-content">	
-										<div class="content">
-											<table class="table ">
-												<tr>
-													<td><label class="label">Indicadora de Acreditados Sin Atraso (<b>BK12_CLEAN</b>): </label></td>
-													<td style="width:25%;"><input type="number" step="0.001" v-model="BK12_CLEAN" class="input" /> </td>
-												</tr>
-												<tr>
-													<td><label class="label">Monto máximo de crédito otorgado por instituciones financieras bancarias en los últimos 12 meses. (<b>BK12_MAX_CREDIT_AMT</b>): </label></td>
-													<td><input type="number" step="0.001" v-model="BK12_MAX_CREDIT_AMT" class="input" /> </td>
-												</tr>
-												<tr>
-													<td><label class="label">Deuda Total de Acreditado (<b>DEUDA_TOT</b>): </label></td>
-													<td><input type="number" step="0.001" v-model="deuda_total" class="input" /> </td>
-												</tr>
-												<tr>
-													<td><label class="label">Antigüedad en sociedad de información crediticia (<b>MONTHS_ON_FILE_BANKING</b>): </label></td>
-													<td><input type="number" step="0.001" v-model="MONTHS_ON_FILE_BANKING" class="input" /> </td>
-												</tr>
-												<tr>
-													<td><label class="label">Deuda a Corto Plazo Total (instituciones Bancarias y No Bancarias: <b>BK_DEUDA_CP+NBK_DEUDA_CP</b>): </label></td>
-													<td><input type="number" step="0.001" v-model="deuda_cortoplazo" class="input" /></td>
-												</tr>
-											</table>
+								<div class="columns card">									
+									<div class="column">
+										<header class="header-sec-card">
+											<p class="card-header-title title-color">Segmento califica</p>
+										</header>
+										<div class="card-content">	
+											<div class="content">
+												<table class="table ">
+													<tr>
+														<td><label class="label">Indicadora de Acreditados Sin Atraso (<b>BK12_CLEAN</b>): </label></td>
+														<td style="width:25%;"><input type="number" step="0.001" v-model="BK12_CLEAN" class="input" /> </td>
+													</tr>
+													<tr>
+														<td><label class="label">Monto máximo de crédito otorgado por instituciones financieras bancarias en los últimos 12 meses. (<b>BK12_MAX_CREDIT_AMT</b>): </label></td>
+														<td><input type="number" step="0.001" v-model="BK12_MAX_CREDIT_AMT" class="input" /> </td>
+													</tr>
+													<tr>
+														<td><label class="label">Deuda Total de Acreditado (<b>DEUDA_TOT</b>): </label></td>
+														<td><input type="number" step="0.001" v-model="deuda_total" class="input" /> </td>
+													</tr>
+													<tr>
+														<td><label class="label">Antigüedad en sociedad de información crediticia (<b>MONTHS_ON_FILE_BANKING</b>): </label></td>
+														<td><input type="number" step="0.001" v-model="MONTHS_ON_FILE_BANKING" class="input" /> </td>
+													</tr>
+													<tr>
+														<td><label class="label">Deuda a Corto Plazo Total (instituciones Bancarias y No Bancarias: <b>BK_DEUDA_CP+NBK_DEUDA_CP</b>): </label></td>
+														<td><input type="number" step="0.001" v-model="deuda_cortoplazo" class="input" /></td>
+													</tr>
+												</table>
+											</div>
+										</div>
+									</div>									
+								</div>
+								<div class="columns card">
+									<div class="column">										
+										<header class="header-sec-card">
+											<p class="card-header-title title-color"> Experiencia crediticia</p>
+										</header>
+										<div class="card-content">	
+											<div class="content">								
+												<table class="table">												
+													<tbody>
+														<tr>
+															<td>
+																<div class="field">
+																  <input class="is-checkradio" v-model="exp_creditos_largos" id="exp_creditos_largos" type="checkbox" name="exp_creditos_largos">
+																  <label for="exp_creditos_largos"><b>Tiene experiencia con créditos simples > 48 meses?</b></label>
+																</div>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 							<div class="column">
-								<div class="card">
-									<header class="header-sec-card">
-										<p class="card-header-title title-color">Reporte de buró de crédito (Resumen)</p>
-									</header>
-									<div class="card-content">	
-										<div class="content">								
-											<table class="table ">
-												<thead>
-													<tr>
-														<th >Tipo de crédito</th>
-														<th >Número de créditos</th>
-														<th >Monto original</th>
-														<th >Monto vigente</th>
-													</tr>								
-												</thead>
-												<tbody>
-													<tr>
-														<td><b>Revolvente</b></td>
-														<td><input type="number"  v-model="num_cred_act_revol" class="input" /></td>
-														<td><input type="number" step="0.001" v-model="sal_orig_cred_act_revol" class="input" /></td>
-														<td><input type="number" step="0.001" v-model="sal_vig_cred_act_revol" class="input" /></td>
-													</tr>
-													<tr>
-														<td><b>Factorajes</b></td>
-														<td><input type="number"  v-model="num_cred_act_fact" class="input" /></td>
-														<td><input type="number" step="0.001" v-model="sal_orig_cred_act_fact" class="input" /></td>
-														<td><input type="number" step="0.001" v-model="sal_vig_cred_act_fact" class="input" /></td>
-													</tr>
-													<tr>
-														<td><b>Arrendamientos</b></td>
-														<td><input type="number"  v-model="num_cred_act_arren" class="input" /></td>
-														<td><input type="number" step="0.001" v-model="sal_orig_cred_act_arren" class="input" /></td>
-														<td><input type="number" step="0.001" v-model="sal_vig_cred_act_arren" class="input" /></td>
-													</tr>
-													<tr>
-														<td><b>Simples</b></td>
-														<td><input type="number"  v-model="num_cred_act_simp" class="input" /></td>
-														<td><input type="number" step="0.001" v-model="sal_orig_cred_act_simp" class="input" /></td>
-														<td><input type="number" step="0.001" v-model="sal_vig_cred_act_simp" class="input" /></td>
-													</tr>
-												</tbody>
-											</table>
+								<div class="columns card">		
+									<div class="column">										
+										<header class="header-sec-card">
+											<p class="card-header-title title-color">Reporte de buró de crédito (Resumen)</p>
+										</header>
+										<div class="card-content">	
+											<div class="content">								
+												<table class="table ">
+													<thead>
+														<tr>
+															<th >Tipo de crédito</th>
+															<th >Número de créditos</th>
+															<th >Monto original</th>
+															<th >Monto vigente</th>
+														</tr>								
+													</thead>
+													<tbody>
+														<tr>
+															<td><b>Revolvente</b></td>
+															<td><input type="number"  v-model="num_cred_act_revol" class="input" /></td>
+															<td><input type="number" step="0.001" v-model="sal_orig_cred_act_revol" class="input" /></td>
+															<td><input type="number" step="0.001" v-model="sal_vig_cred_act_revol" class="input" /></td>
+														</tr>
+														<tr>
+															<td><b>Factorajes</b></td>
+															<td><input type="number"  v-model="num_cred_act_fact" class="input" /></td>
+															<td><input type="number" step="0.001" v-model="sal_orig_cred_act_fact" class="input" /></td>
+															<td><input type="number" step="0.001" v-model="sal_vig_cred_act_fact" class="input" /></td>
+														</tr>
+														<tr>
+															<td><b>Arrendamientos</b></td>
+															<td><input type="number"  v-model="num_cred_act_arren" class="input" /></td>
+															<td><input type="number" step="0.001" v-model="sal_orig_cred_act_arren" class="input" /></td>
+															<td><input type="number" step="0.001" v-model="sal_vig_cred_act_arren" class="input" /></td>
+														</tr>
+														<tr>
+															<td><b>Simples</b></td>
+															<td><input type="number"  v-model="num_cred_act_simp" class="input" /></td>
+															<td><input type="number" step="0.001" v-model="sal_orig_cred_act_simp" class="input" /></td>
+															<td><input type="number" step="0.001" v-model="sal_vig_cred_act_simp" class="input" /></td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="columns card">						
+									<div class="column">										
+										<header class="header-sec-card">
+											<p class="card-header-title title-color">Generales</p>
+										</header>
+										<div class="card-content">	
+											<div class="content">								
+												<table class="table">												
+													<tbody>
+														<tr>
+															<td><label class="label">Fecha consulta de buró: </label></td>
+															<td><input type="date" v-model="fecha_consulta" class="input" /></td>
+														</tr>
+														<tr>
+															<td><label class="label">Calificación buro: </label></td>
+															<td><input type="text"  v-model="calif_buro" class="input" /></td>
+														</tr>
+														<tr>
+															<td><label class="label">Línea más alta: </label></td>
+															<td><input type="number" step="1"  v-model="linea_mas_alta" class="input" /></td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>					
-					</div>
-					<div class="columns">
-						<div class="column is-6">
-							<div class="card">
-								<header class="header-sec-card">
-									<p class="card-header-title title-color"> Experiencia crediticia</p>
-								</header>
-								<div class="card-content">	
-									<div class="content">								
-										<table class="table">												
-											<tbody>
-												<tr>
-													<td>
-														<div class="field">
-														  <input class="is-checkradio" v-model="exp_creditos_largos" id="exp_creditos_largos" type="checkbox" name="exp_creditos_largos">
-														  <label for="exp_creditos_largos"><b>Tiene experiencia con créditos simples > 48 meses?</b></label>
-														</div>
-													</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="column is-6">
-							<div class="card">
-								<header class="header-sec-card">
-									<p class="card-header-title title-color">Generales</p>
-								</header>
-								<div class="card-content">	
-									<div class="content">								
-										<table class="table">												
-											<tbody>
-												<tr>
-													<td><label class="label">Fecha consulta de buró: </label></td>
-													<td><input type="date" v-model="fecha_consulta" class="input" /></td>
-												</tr>
-												<tr>
-													<td><label class="label">Calificación buro: </label></td>
-													<td><input type="text"  v-model="calif_buro" class="input" /></td>
-												</tr>
-												<tr>
-													<td><label class="label">Línea más alta: </label></td>
-													<td><input type="number" step="1"  v-model="linea_mas_alta" class="input" /></td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+					</div>				
 				</div>
 			</div>
 			<results v-bind="$props"></results>			
